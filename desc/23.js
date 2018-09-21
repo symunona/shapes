2e16e9d3c18783bb80e7c183cb85296b94ada127
symunona <symunona@gmail.com>
2018-09-21 14:27:10 -0400

    yeah.

---
+++ desc/23.js
@@ -0,0 +1,43 @@
+/**
+ * #23
+ */
+
+module.exports = function (d) {
+
+    let r = d.h / 3
+    let c = { x: d.cx, y: d.cy }
+
+    let triangle = d.poly(3, r, c, Math.PI / 3)
+    triangle.reverse()
+    let g = d.append('g').attr('fill-rule', 'evenodd')
+
+    let cx = d.cx, cy = d.cy
+
+    let n = 13
+
+    // pick one point
+    let a = -1 / 3 * Math.PI // -120deg
+    let da = 2 / 3 * Math.PI
+    let ref = { x: cx + (Math.sin(a) * r), y: cy + (Math.cos(a) * r) }
+
+    let from = { x: cx + (Math.sin(a + da) * r), y: cy + (Math.cos(a + da) * r) }
+    let to = { x: cx + (Math.sin(a - da) * r), y: cy + (Math.cos(a - da) * r) }
+
+    let dslide = d.distance(from, to) / n;
+    let points = [ref]
+
+    for (let i = 0; i < n; i += 2) {
+        points.push(d.linear(from, to, i / n))
+        points.push(d.linear(from, to, (i + 1) / n))
+        points.push(ref)
+
+    }
+
+    g.append('path')
+        .attr('d',
+            // d.lineD(d.d3.curveLinearClosed)(triangle) + 
+            d.lineD(d.d3.curveLinearClosed)(points))
+        .attr('fill', d.c[3])
+
+    d.save()
+}
--- frame.js
+++ frame.js
@@ -34,8 +34,8 @@ if (process.argv.length > 2) {
     // Hahh!
     files = files.filter((fn) => fn.substr(-3) === '.js')
     files = files.map((fn) => Number(fn.substr(0, fn.length - 3)))
-    files = files.sort((a,b)=>a>b?1:a<b?-1:0)
-    files = files.map((fn) => String(fn)+'.js')
+    files = files.sort((a, b) => a > b ? 1 : a < b ? -1 : 0)
+    files = files.map((fn) => String(fn) + '.js')
 
     for (let i = 0; i < files.length; i++) {
         let no = files[i].substr(0, files[i].length - 3)
@@ -66,7 +66,9 @@ function init(no, _label) {
     dr.bg = colors[1]
     dr.label = label
     dr.circlePath = circlePath
-
+    dr.linear = linear
+    dr.distance = distance
+    dr.mark = mark.bind(this, dr)
     dr.attr('viewport-fill', '#000')
 
     let defs = dr.append('defs')
@@ -80,7 +82,18 @@ function init(no, _label) {
     return dr
 }
 
+function distance(a, b) {
+    return Math.sqrt((
+        (a.x - b.x) * (a.x - b.x)) +
+        ((a.y - b.y) * (a.y - b.y)))
+}
 
+function linear(from, to, n) {
+    return {
+        x: from.x + ((to.x - from.x) * n),
+        y: from.y + ((to.y - from.y) * n)
+    }
+}
 
 function poly(n, r, offset, angleOffset) {
     let da = Math.PI * 2 / n
@@ -92,6 +105,16 @@ function poly(n, r, offset, angleOffset) {
         y: (Math.cos(da * i + angleOffset) * r) + offset.y
     })
     return ret
+}
+
+function mark(d, p, color) {
+    d.append('circle')
+        .attrs({
+            fill: color || 'red',
+            cx: p.x,
+            cy: p.y,
+            r: 3
+        })
 }
 
 function save(imageIndex, d, _label) {
@@ -138,13 +161,13 @@ function loadJs(no, defs) {
     try {
         js = fs.readFileSync(`desc/junk/${no}.js`, 'utf8')
     } catch (e) { }
-    if (js){
+    if (js) {
         defs.append('script')
             .attr('type', 'text/javascript')
             // .attr('xlink:href', `desc/junk/${no}.js`)
             .text(`<![CDATA[\n${js}\n]]>`)
     }
-    
+
 }
 
 function lineD(c) {
@@ -152,5 +175,5 @@ function lineD(c) {
         .x(function (d) { return d.x; })
         .y(function (d) { return d.y; })
         .curve(c || D3Node.d3.curveCardinalClosed);
-        
+
 }
