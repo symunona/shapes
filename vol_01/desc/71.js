54af4ab6eae500033d0b47ae3a805fa27fb7735c
Bendeguz <symunona@gmail.com>
2020-11-23 11:57:54 +0100

    New candidate: gradiant 71

--- vol_01/desc/71.js
+++ vol_01/desc/71.js
@@ -1,8 +1,6 @@
 /**
  * #71
  * New fill style gradient.
- *
- * TODO
  */
 module.exports = function (d) {
 
@@ -19,27 +17,41 @@ module.exports = function (d) {
                 .append('path')
                 .attr('d', unit({ x: sizeX / 2, y: sizeY / 3 }, d))
                 .attr('transform', d.m({ x: sizeX * (xs + 1.5), y: sizeY * (ys + 1.5) }))
-                .attr('fill', 'url(#stripes-' + (xs + ys) + ')')
+                .attr('fill', 'url(#stripes' + (xs + ys) + ')')
 
         }
     }
 
     for (let i = 0; i < gridX; i++) {
-        makePattern(d.defs, i, gridX)
+        makePattern(d.defs, i, gridX, d)
     }
 
     d.save('gradient #2')
 }
 
-function makePattern(defs, n, all) {
-    defs.append('pattern')
-        .attr('id', 'stripes-' + n)
+function makePattern(defs, n, all, d) {
+    let pattern = defs.append('pattern')
+        .attr('id', 'stripes' + n)
         .attr('patternUnits', 'userSpaceOnUse')
         .attr('width', 4)
         .attr('height', 4)
+    pattern
         .append('path')
-        .attr('d', 'M-1,1 l2,-2 M0,4 l4,-4 M3,5 l2,-2')
-        .attr('style', 'stroke-width: ' + ((n + 1) * (3 / all)))
+        .attr('d', // 'M-1,1 l2,-20 M0,40 l4,-40 M3,5 l2,-2') //
+            d.lineD(d.d3.curveLinear)([
+            {x: -2, y: 0},
+            {x: 4, y: 6}
+        ]))
+        .attr('style', 'line-cap: butt; stroke: #333; stroke-width: ' + ((n + 1) * (3 / all)))
+        .attr('class', 'fore')
+    pattern
+        .append('path')
+        .attr('d', // 'M-1,1 l2,-20 M0,40 l4,-40 M3,5 l2,-2') //
+            d.lineD(d.d3.curveLinear)([
+            {x: 0, y: -2},
+            {x: 6, y: 4}
+        ]))
+        .attr('style', 'line-cap: butt; stroke: #333; stroke-width: ' + ((n + 1) * (3 / all)))
         .attr('class', 'fore')
 }
 
